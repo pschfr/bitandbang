f6138470d6657c17dd1dc21d93ef81d5229d5213
Paul Schaefer <yeppaul2@gmail.com>
2018-12-20 21:14:07 -0500

    Removed boxen dependency and devDependencies; adjusted spacing

--- bin/card.js
+++ bin/card.js
@@ -3,41 +3,33 @@
 
 // Pull in our modules
 const chalk = require('chalk')
-const boxen = require('boxen')
-
-// Define options for Boxen
-const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round'
-}
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Paul Schaefer'),
+  name: chalk.white.bold('Paul Schaefer'),
   handle: chalk.cyan('pschfr'),
   work: chalk.white('Front and Back-end Developer'),
   twitter: chalk.cyan('https://twitter.com/pschfr'),
   github: chalk.cyan('https://github.com/pschfr'),
   web: chalk.cyan('https://paulmakesthe.net/'),
   npx: chalk.white('npx pschfr'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:')
+  labelWork: chalk.white.bold('Work:'),
+  labelTwitter: chalk.white.bold('Twitter:'),
+  labelGitHub: chalk.white.bold('GitHub:'),
+  labelWeb: chalk.white.bold('Web:'),
+  labelCard: chalk.white.bold('Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
+const working = `${data.labelWork}    ${data.work}`
+const twittering = `${data.labelTwitter} ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const webing = `${data.labelWeb}     ${data.web}`
+const carding = `${data.labelCard}    ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + webing + newline + newline + carding + newline
 
-console.log(chalk.green(boxen(output, options)))
+console.log(output)
