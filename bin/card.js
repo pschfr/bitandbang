6a70bd375a5af95e927084271e45b9347257d1d8
Paul Schaefer <yeppaul2@gmail.com>
2018-12-23 21:02:00 -0500

    Simplified strings for less variables and easier maintenance

--- bin/card.js
+++ bin/card.js
@@ -4,30 +4,13 @@
 // Pull in our colors from kleur.
 const { white, cyan, bold } = require('kleur')
 
-// Text + color definitions
-const data = {
-  name: white().bold('Paul Schaefer'),
-  handle: cyan('pschfr'),
-  work: white('Front and Back-end Developer'),
-  twitter: cyan('https://twitter.com/pschfr'),
-  github: cyan('https://github.com/pschfr'),
-  web: cyan('https://paulmakesthe.net/'),
-  npx: white('npx pschfr'),
-  labelWork: white().bold('Work:'),
-  labelTwitter: white().bold('Twitter:'),
-  labelGitHub: white().bold('GitHub:'),
-  labelWeb: white().bold('Web:'),
-  labelCard: white().bold('Card:')
-}
-
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}    ${data.work}`
-const twittering = `${data.labelTwitter} ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const webing = `${data.labelWeb}     ${data.web}`
-const carding = `${data.labelCard}    ${data.npx}`
+const heading = white().bold('Paul Schaefer') + ' ' + cyan('pschfr') + '\n\n'
+const working = white().bold('Work:') + '    ' + white('Front and Back-end Developer') + '\n'
+const twitter = white().bold('Twitter:') + ' ' + cyan('https://twitter.com/pschfr') + '\n'
+const github  = white().bold('GitHub:') + '  ' + cyan('https://github.com/pschfr') + '\n'
+const website = white().bold('Web:') + '     ' + cyan('https://paulmakesthe.net/') + '\n\n'
+const card    = white().bold('Card:') + '    ' + white('npx pschfr')
 
 // Output it all!
-console.log(newline + heading + newline + newline + working + newline + twittering + newline + githubing + newline + webing + newline + newline + carding + newline)
+console.log('\n' + heading + working + twitter + github + website + card + '\n')
